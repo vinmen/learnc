ebdac98efe54d44c153a0f2007f93c01c76a9600
vinmen <vinumenon@gmail.com>
2026-01-22 11:42:11 -0500

    Added ktu struct malloc

--- ktu_employee_details.c
+++ ktu_employee_details.c
@@ -1,23 +1,87 @@
 
-#include <stdio.h>
+/*
+    Dynamic employee recording and retreval using malloc and free.
+*/
 
-struct Employee {
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+#define NAME_BUF_SIZE 10
+
+typedef struct {
     int id;
-    char name[20];
-    char email[50];
-};
+    char* name;    
+} Employee;
 
 int main() {
 
-    struct Employee employee;
-    
-    printf("Enter employee details:");
-    printf("\nid: ");
-    scanf("%d", &employee.id);
-    printf("\nName: ");
-    scanf("%s", &employee.name);
-    printf("\nEmail: ");
-    scanf("%s", &employee.email);
+    int emplopyee_count = 0;
+    int block_size = 2;
+    char key = '\0';
+    Employee* employee = NULL;
+    int memory_added = 0;
+
+    while(key != 'q') {
+
+        printf("Enter command :");
+        key = getch();
+
+        if(key == 'a') {
+            if(emplopyee_count == 0) {
+                employee = (Employee *) malloc(block_size * sizeof(Employee));
+                memory_added = 1;                
+            }
+            else if(emplopyee_count == block_size) {
+                block_size = block_size + 2;  
+                employee = (Employee *) realloc(employee, block_size * sizeof(Employee));                               
+                memory_added = 1;
+            }
+            
+            if(memory_added) {
+                if(employee == NULL) {                   
+                    exit(EXIT_FAILURE);
+                }                
+                for(int i = emplopyee_count; i < block_size; i++) {
+                    employee[i].name = malloc(NAME_BUF_SIZE * sizeof(char));
+                    if (employee[i].name == NULL) {                        
+                        free(employee);
+                        exit(EXIT_FAILURE);
+                    }
+                }
+                memory_added = 0;
+            }
+            
+            printf("\nEnter employee id, name:");
+            scanf("%d %s", &employee[emplopyee_count].id, employee[emplopyee_count].name);
+            emplopyee_count++;
+        }
+        else if(key == 'p') {            
+            int id;
+            printf("\nEnter employee id :");
+            scanf("%d", &id);
+
+            for(int i = 0; i < emplopyee_count; i++) {
+                if (id != 0 && employee[i].id == id) {
+                    printf("\nEmployee's name: %s\n", employee[i].name);
+                    break;
+                }
+                else if(id == 0) {
+                     printf("%d\t%s\n", employee[i].id, employee[i].name);
+                }
+            }
+        }      
+        else if(key == 'h') {
+            printf("\nOptions:\n a - Add Employee\n p - Print Employee\n h - Help\n q - Quit\n");
+        }
+        else if(key != 'q') {
+            for(int i = 0; i < block_size; i++) {
+                 free(employee[i].name);
+            }            
+            free(employee);
+            printf("\nh - Help.\n");
+        }
+    }
 
     return 0;
 }
